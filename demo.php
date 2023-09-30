302fdc20c2235bf1e190d5453bb7380e70fc6353
Agnes Nazarro <agnesnazarro@gmail.com>
2023-09-30 16:09:42 +0800

    upload live server demo file

---
+++ demo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+<body>
+    <h1>PHP Testing</h1>
+    <h2>Testing</h2>
+    <h3>Testing 1</h3>
+    <h4>Testing 2</h4>
+    <h5>Testing 3</h5>
+    
+    <?php 
+        echo "Hello, World <br>"; 
+        echo "Testing 1 <br>";
+        echo "Testing 2 <br>";
+        echo "Testing 3 <br>";
+    ?>
+</body>
+</html>
