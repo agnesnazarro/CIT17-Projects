3af167159fc7bbc7422a8e6a2cd62cc60a730c51
agnesnazarro <agnesnazarro@gmail.com>
2023-10-17 18:11:15 +0800

    upload new demo2 file

---
+++ demo2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo 2</title>
+</head>
+<body>
+    <?php 
+        echo "Personal Info";
+        
+        $name = "Agnes Nazarro";
+        $emailAdd = "agnesnazarro@gmail.com";
+        $address = "Baguio City";
+
+        echo "<br> Name: $name <br>";
+        echo "Email Address: $emailAdd";
+        echo "Address: $address";
+    ?>
+</body>
+</html>
